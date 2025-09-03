c70b16ce74b66fb3a57511cd353140dc12eee117
lakhanchaudhary25 <optimasprime883@gmail.com>
2025-09-03 22:37:17 +0530

    added cursor and scrolling animations

---
+++ script.js
@@ -0,0 +1,14 @@
+gsap.to("#nav",{
+    backgroundColor:"black",
+    height:"145px",
+    duration:0.5,
+    
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        start:"top-10%",
+        
+    
+    } 
+
+})
