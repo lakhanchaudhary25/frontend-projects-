5749380ef4eb197dc7897997901f6c1f41c075b1
lakhanchaudhary25 <optimasprime883@gmail.com>
2025-09-10 11:40:23 +0530

    added cursor

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 gsap.to("#nav",{
     backgroundColor:"black",
-    height:"145px",
+    height:"125px",
     duration:0.5,
     
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        start:"top-10%",
-        
-    
+        start:"top -10%",
+        end:"top -8%",
+     markers:"true",
+     scrub:2,
     } 
+
+})
+gsap.to("#main",{
+    backgroundColor:"black",
+
+    scrollTrigger:{
+    triger:"#main",
+    scroller:"body",
+    start:"top -26%",
+    markers:"true",
+    end:"top -70%",
+    scrub:2,
+}
+})
+Document.queryselector
+document.addEventListener("mousemove",function(dets){
 
 })
